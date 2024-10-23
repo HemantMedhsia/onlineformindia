e5fb182c50d900d17b1fc53453229f7b86c66b81
Hemant Medhsia <hemantmedhsia@gmail.com>
2024-10-23 14:14:03 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuButton = document.getElementById('mobile-menu-button');
-    const mobileMenu = document.getElementById('mobile-menu');
-  
-    menuButton.addEventListener('click', () => {
-      mobileMenu.classList.toggle('hidden');
-    });
-  });
-  
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+let isOpen = false;
+
+menuToggle.addEventListener("click", () => {
+  if (!isOpen) {
+    gsap.to(mobileMenu, { duration: 0.5, x: 0 });
+    mobileMenu.classList.remove("hidden");
+    isOpen = true;
+  } else {
+    gsap.to(mobileMenu, { duration: 0.5, x: "100%" });
+    setTimeout(() => {
+      mobileMenu.classList.add("hidden");
+    }, 500);
+    isOpen = false;
+  }
+});
